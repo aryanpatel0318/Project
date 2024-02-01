c6c5af002a9d3059e0a0da6064ae8115f0cad5cb
aryanpatel0318 <99696854+aryanpatel0318@users.noreply.github.com>
2024-02-01 15:15:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,64 +3,68 @@ let ballY;
 let obstacleX;
 let obstacleY;
 let score = 0;
-let gameOver = false;
+let gameOver = false; 
+
+// declares variables
+
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(400, 400); // creates canvas
     startGame();
 }
 
+
 function startGame() {
     ballX = width / 2;
-    ballY = height / 2;
-    obstacleX = random(width);
-    obstacleY = 0;
-    score = 0;
-    gameOver = false;
+    ballY = height / 2; // creates ball
+    obstacleX = random(width); // creates obstacle at a random location on the x-axis
+    obstacleY = 0; // sets the obstaacle to have  y-coordinate of zero
+    score = 0; //sets the score initially at zero
+    gameOver = false; //makes it so the game pauses when you hit an obstacle
     loop();
 }
 
 function draw() {
-    background(220);
+    background(220); //changes the background color to light grey
     
     // Ball
-    fill(150);
-    ellipse(ballX, ballY, 30, 30);
+    fill(150); //makes the ball colour to dark grey
+    ellipse(ballX, ballY, 30, 30); //the width and height of the ball
     
     // Obstacle
-    fill(255, 0, 0);
-    rect(obstacleX, obstacleY, 50, 50);
-    obstacleY += 5;
+    fill(255, 0, 0); //makes the obstacle red
+    rect(obstacleX, obstacleY, 50, 50) //sets the width and height of the obstacle 
+    obstacleY += 5; //makes the obstacle incremently increase by 5
     
-    if (obstacleY > height) {
+    if (obstacleY > height) { //if the obstacle exceeds the height of the canvas it restarts to the top of the canvas
         obstacleY = 0;
-        obstacleX = random(width);
-        score++;
+        obstacleX = random(width); //randomly goes to any point on the x-axis upon restart
+        score++; //the score will increase by 1
     }
     
     
     // Check collision
-    if (dist(ballX, ballY, obstacleX + 25, obstacleY + 25) < 40) {
-        gameOver = true;
-        noLoop();
+    if (dist(ballX, ballY, obstacleX + 25, obstacleY + 25) < 40) { //checks if the ball is less than 40 from the obstacle
+        gameOver = true; //pauses the game when the ball hits the obstacle
+        noLoop(); //Makes the game not loop
     }
     
     // Score
     fill(0);
     textSize(16);
-    text("Score: " + score, 10, 30);
+    text("Score: " + score, 10, 30); 
     
     if (!gameOver) {
         // Control the ball
-        if (keyIsDown(LEFT_ARROW)) {
+        if (keyIsDown(LEFT_ARROW)) { //if you click the left arrow the ball moves left
             ballX -= 4;
-        } else if (keyIsDown(RIGHT_ARROW)) {
+        } else if (keyIsDown(RIGHT_ARROW)) { //if you click the right arrow the ball moves right
             ballX += 4;
         }
 
-        if (keyIsDown(UP_ARROW)) {
+        if (keyIsDown(UP_ARROW)) { //if you click the up arrow the ball moves up
             ballY -= 4;
-        } else if (keyIsDown(DOWN_ARROW)) {
+        } else if (keyIsDown(DOWN_ARROW)) { //if you click the down arrow the ball moves down
             ballY += 4;
         }
     }
